2936a510cbb4f69a1b50f702f3f7111d676f4002
Rene Cocom <fasnimo@gmail.com>
2019-03-14 04:52:15 +0000

    Automatically backed up by Learn

--- meal_choice.rb
+++ meal_choice.rb
@@ -1,5 +1,5 @@
 # Define a method meal_choice that returns the meal_choice that was
 # passed into it and defaults to meat.
 def meal_choice(eater = "meat") 
-  puts "I am on a #{vegan} diet!"
+  puts "I am on a #{eater} diet!"
 end
